e13b2b88ada8e3ab84eaf2573498dc5bfcf38324
arielarce <ariel@conexa.ai>
2023-12-03 11:13:04 -0300

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Post, Body, Get, Delete, Patch, Param, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { createUserDto } from './dto/create-user.dto';
+import { updateUserDto } from './dto/update-user.dto';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private usersService: UsersService) {}
+
+    @Get()
+    getUsers(): Promise<User[]> {
+        return this.usersService.getUsers();
+    }
+
+    // ParseIntPipe: cambia el tipo de dato
+    @Get(':id')
+    getUser(@Param('id', ParseIntPipe) id: number) {
+        console.log(id)
+        console.log(typeof id)
+        return this.usersService.getUser(id);
+    }
+
+    @Post()
+    createUser(@Body() newUser: createUserDto) {
+      return this.usersService.createUser(newUser);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.deleteUser(id)
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id', ParseIntPipe) id: number, @Body() user: updateUserDto) {
+        return this.usersService.updateUser(id, user)
+    }
+
+}
